6e5e80e54115f835269fcc29fb96d71a4e78bd07
Samro Stephen <samrostephen0305@gmail.com>
2026-03-21 22:26:15 +0530

    fix db crash and connection handling

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -24,18 +24,7 @@ exports.createProduct = async (req, res) => {
 
 exports.getProducts = async (req, res) => {
   try {
-    const { q = "" } = req.query;
-
-    const products = await prisma.product.findMany({
-      where: q
-        ? {
-            name: {
-              contains: q,
-            },
-          }
-        : {},
-    });
-
+    const products = await prisma.product.findMany();
     res.json(products);
   } catch (error) {
     console.error("GET PRODUCTS ERROR:", error);
